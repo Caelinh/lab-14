6e219a8b6903dc14541abbfceef7952fba509cf2
Caelinh <Caelinhargrave@gmail.com>
2022-06-23 20:05:23 -0500

    Added more functions

--- js/cart.js
+++ js/cart.js
@@ -6,6 +6,7 @@ const table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
 let cart;
 
+
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
   cart = new Cart(cartItems);
@@ -19,21 +20,40 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let parent = document.querySelector("tbody");
+  let child = parent.lastElementChild;
+  while (child) {
+    parent.removeChild(child);
+    child = parent.lastElementChild;
+  }
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
+  let body = document.querySelector('tbody');
+  for (let i = 0; i < cart.items.length; i++) {
+    let tableRow = document.createElement('tr');
+    tableRow.textContent = cart.items.product.name;
+    body.appendChild(tableRow);
 
-  // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
+
+    let tableData = document.createElement('td');
+    tableRow.appendChild(tableData);
+    tableData.textContent(removeItemFromCart, cartItems.quantity, cartItems);
+
+    // TODO: Iterate over the items in the cart
+    // TODO: Create a TR
+    // TODO: Create a TD for the delete link, quantity,  and the item
+    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
+  }
 }
-
 function removeItemFromCart(event) {
+
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
   // TODO: Save the cart back to local storage
--- js/catalog.js
+++ js/catalog.js
@@ -11,8 +11,10 @@ function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
-  for (let i in Product.allProducts) {
-
+  for (let i= 0; i < Product.allProducts.length; i++) {
+    let option = document.createElement('option');
+    option.text = Product.allProducts[i].name;
+    selectElement.add(option);
   }
 
 }
@@ -23,7 +25,7 @@ function populateForm() {
 function handleSubmit(event) {
 
   // TODO: Prevent the page from reloading
-
+event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
   cart.saveToLocalStorage();
@@ -32,16 +34,30 @@ function handleSubmit(event) {
 
 }
 
+  
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  let product;
+  let options = document.querySelectorAll("option"); 
+  options.forEach(element => {
+    element.addEventListener('selected', function(event){
+      if (event = true){
+        product = event.value
+      }
+    })
+  }
+  let quantity = document.getElementById('quantity');
+
   // TODO: get the quantity
+
   // TODO: using those, add one item to the Cart
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() { }
-
+// function updateCounter() { }
+// let counter = getElementById("itemCount");
+// counter.textContent = Cart.length;
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
